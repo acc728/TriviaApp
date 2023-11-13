5fd185d2f12a8a4af1e43ddc0881adb046dd5219
Adrian Camacho Capilla <adrian.cam.cap@gmail.com>
2023-11-12 23:46:39 -0800

    Initial Commit

---
+++ Trivia App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Trivia App
+//
+//  Created by user242582 on 12/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Trivia App/Trivia_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Trivia_AppApp.swift
+//  Trivia App
+//
+//  Created by user242582 on 12/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Trivia_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
