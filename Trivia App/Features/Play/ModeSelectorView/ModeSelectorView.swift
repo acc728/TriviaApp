dfe797dc67220f598e9707878a787a4a5f80fb92
Adrian Camacho Capilla <adrian.cam.cap@gmail.com>
2023-11-14 01:05:24 -0800

    ViewModel and quiz reestructured and continued

---
+++ Trivia App/Features/Play/ModeSelectorView/ModeSelectorView.swift
@@ -0,0 +1,18 @@
+//
+//  ModeSelectorView.swift
+//  Trivia App
+//
+//  Created by user242582 on 13/11/23.
+//
+
+import SwiftUI
+
+struct ModeSelectorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ModeSelectorView()
+}
