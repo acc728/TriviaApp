75f4af00d89dda668b4f011e6c8a99276134f81a
Adrian Camacho Capilla <adrian.cam.cap@gmail.com>
2023-11-14 07:35:59 -0800

    CardView Created

--- Trivia App/Components/CardView.swift
+++ Trivia App/Components/CardView.swift
@@ -9,23 +9,58 @@ import SwiftUI
 
 struct CardView: View {
     var title: String
-    var message: String
+    var systemName: String
+    var colorsGradient: [Color]
     
     var body: some View {
         HStack {
-            
+            VStack(alignment: .leading) {
+                Spacer()
+                Image(systemName: systemName)
+                    .resizable()
+                    .frame(width: 40, height: 40)
+                    .foregroundStyle(.white)
+                    
+                Text(title)
+                    .bold()
+                    .font(.largeTitle)
+                    .foregroundStyle(.white)
+                    
+            }
+            .padding()
+            Spacer()
         }
-        VStack(spacing: 20) {
-            Text(title)
-            Text(message)
-        }
-        .background(Color("AccentColor"))
+        .frame(maxWidth: .infinity, maxHeight: 200)
+        .background(
+            LinearGradient(
+                gradient: Gradient(colors: colorsGradient),
+                startPoint: .topLeading,
+                endPoint: .bottomTrailing))
+        .clipShape(RoundedRectangle(cornerRadius: 10))
+        .shadow(radius: 5, x: 3, y: 3)
     }
 }
 
 #Preview {
     CardView(
-        title: "Titulo",
-        message: "Mensaje"
+        title: "Survival",
+        systemName: "trophy.fill",
+        colorsGradient: [Color(hex: 0xFF4470),
+                 Color(hex: 0xF4698A),
+                 Color(hex: 0xF89051),
+                 Color(hex: 0xFAB168)]
+        
+        /* Gradient Red
+         [Color(hex: 0xFF4470),
+          Color(hex: 0xF4698A),
+          Color(hex: 0xF89051),
+          Color(hex: 0xFAB168)]
+         */
+        
+        /* Gradient Blue
+         [Color(hex: 0x2C3EE2),
+          Color(hex: 0x4B69DE),
+          Color(hex: 0x1791D6),
+          Color(hex: 0x17AAD6)]*/
     )
 }
