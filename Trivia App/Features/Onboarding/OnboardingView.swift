f92f3f8c62ae00807b5d0e23915ad7dcddd0e323
Adrian Camacho Capilla <adrian.cam.cap@gmail.com>
2023-11-28 02:14:36 -0800

    Onboarding started

--- Trivia App/Features/Onboarding/OnboardingView.swift
+++ Trivia App/Features/Onboarding/OnboardingView.swift
@@ -9,7 +9,56 @@ import SwiftUI
 
 struct OnboardingView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            VStack(spacing: 20) {
+                HStack {
+                    Text("Welcome user!")
+                        .font(.title)
+                        .fontWeight(.semibold)
+                        .kerning(1.4)
+                    
+                    Spacer()
+                    
+                    Button(action: {
+                        
+                    }, label: {
+                        Text("Skip")
+                            .fontWeight(.semibold)
+                            .foregroundStyle(.black)
+                    })
+                }.padding()
+                
+                Spacer(minLength: 0)
+                
+                Image("trophy")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                
+                Text("Step 1")
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .foregroundStyle(.black)
+                    .padding(.top)
+                
+                Text("Texto de prueba con un texto largo para ver si se ve bien el mensaje")
+                    .fontWeight(.semibold)
+                    .multilineTextAlignment(.center)
+                    .kerning(1.3)
+                
+                Spacer(minLength: 0)
+            }.background(Color("Cantaloupe").ignoresSafeArea())
+        }.overlay(
+        Button(action: {
+            
+        }, label: {
+            Image(systemName: "chevron.right")
+                .font(.title2)
+                .fontWeight(.semibold)
+                .foregroundStyle(.black)
+                .frame(width: 60,height: 60)
+                .background(.white)
+                .clipShape(Circle())
+        }),alignment: .bottom)
     }
 }
 
